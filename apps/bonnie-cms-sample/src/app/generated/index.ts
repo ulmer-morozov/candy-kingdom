04f6e9ef4cdc22981b10eb89afe1783ddafba368
Dmitry Ulmer-Morozov <ulmer.morozov@gmail.com>
2024-02-03 21:16:37 +0400

    fix: transpation textarea

--- apps/bonnie-cms-sample/src/app/generated/index.ts
+++ apps/bonnie-cms-sample/src/app/generated/index.ts
@@ -3,10 +3,10 @@
  * Any changes made to this file can be lost when this file is regenerated.
  */
 
+export * from './text-bone-style';
+export * from './page-list-bone';
+export * from './text-bone';
+export * from './project-page-data';
+export * from './page-list-bone-style';
 export * from './media-bone-style';
 export * from './media-bone';
-export * from './page-list-bone-style';
-export * from './page-list-bone';
-export * from './project-page-data';
-export * from './text-bone-style';
-export * from './text-bone';
