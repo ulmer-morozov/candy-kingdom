3cd707f57e4adaeedb11149cff81f50d9e5228bb
Dmitry Ulmer-Morozov <ulmer.morozov@gmail.com>
2024-02-04 23:07:16 +0400

    chore: clean

--- apps/bonnie-cms-sample/src/app/core/MediaUploadMap.ts
+++ apps/bonnie-cms-sample/src/app/core/MediaUploadMap.ts
@@ -2,7 +2,7 @@ import { MediaType } from '@candy-kingdom/bonnie-cms';
 
 const mediaUploadMap = new Map<MediaType, string>();
 mediaUploadMap.set('image', '/api/admin/upload/image/complex');
-mediaUploadMap.set('video', '/api/admin/upload/image/complex');
+mediaUploadMap.set('video', '/api/admin/upload/video/complex');
 
 export const MediaUploadMap: ReadonlyMap<MediaType, string> = mediaUploadMap;
 
